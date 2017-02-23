90ccf5cd531f0bd77fa4a16a67d5fffa2537c9dc
Alexandre A. Bizri <alexandre@bizri.fr>
2017-02-23 02:10:10 +0100

    First commit

---
+++ iseplive.go
@@ -0,0 +1,60 @@
+package goscrapiseplive
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"github.com/gocarina/gocsv"
+	"io"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+)
+
+type Session struct {
+	Client *http.Client
+}
+
+func Login(username string, password string, client *http.Client) (*Session, error) {
+	// Init
+	sess := &Session{}
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	// Prepare login form
+	loginForm := url.Values{}
+	loginForm.Add("password", password)
+	loginForm.Add("username", username)
+
+	// Make cookiejar
+	cookieJar, _ := cookiejar.New(nil)
+	client.Jar = cookieJar
+
+	// Prepare request
+	req, err := http.NewRequest("POST", "http://iseplive.fr/signin/", bytes.NewBufferString(loginForm.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	// Execute request
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Check if response is OK
+	if resp.StatusCode != 302 {
+		return nil, errors.New(fmt.Sprintf("Status code isn't OK: %d", resp.StatusCode))
+	}
+
+	sess.Client = client
+	return sess, nil
+}
+
+func ExportToCSV(studentList StudentList, writer io.Writer) error {
+	err := gocsv.Marshal(&studentList, writer)
+	return err
+}
---
+++ readme.md
@@ -0,0 +1,26 @@
+# goscrapiseplive
+
+A scrapper for iseplive.fr, a student website
+
+```
+go get github.com/aabizri/goscrapiseplive
+```
+
+## Example:
+
+Start a session
+```golang
+session,err := Login(USERNAME,PASSWORD, &http.Client{})
+```
+
+Scrap the website
+```golang
+studentList,err := sess.GetStudentList()
+```        
+
+Export it in csv format through whatever io.writer you prefer, here with `os.Stdout`
+```golang
+err = ExportToCSV(studentList,os.Stdout)
+```
+
+
