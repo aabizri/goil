509fab5ace04ee5d0c63607ec28592a28f527720
Alexandre A. Bizri <alexandre@bizri.fr>
2017-03-05 22:14:53 +0000

    Add retrieval of login cookie

--- goil.go
+++ goil.go
@@ -15,6 +15,7 @@ package goil
 
 import (
 	"bytes"
+	"errors"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
@@ -55,4 +56,33 @@ func Login(username string, password string, client *http.Client) (*Session, err
 	return sess, nil
 }
 
+// Convenience wrapper around Login & *Session.Cookie
+func LoginRetrieveCookie(username string, password string) (*http.Cookie, error) {
+	// Login
+	sess, err := Login(username, password, &http.Client{})
+	if err != nil {
+		return nil, err
+	}
+
+	// Retrieve cookie
+	return sess.Cookie()
+}
+
+// Retrieve the login cookie
+func (s *Session) Cookie() (*http.Cookie, error) {
+	// Parse the url
+	url, err := url.Parse("http://iseplive.fr")
+	if err != nil {
+		return nil, err
+	}
+
+	// Iterate through the cookies in order to find the right one
+	for _, k := range s.Client.Jar.Cookies(url) {
+		if k.Name == "login" {
+			return k, nil
+		}
+	}
+
+	// If no logincookie was found, return an error
+	return nil, errors.New("No valid login cookie found :(")
 }
