55c22d3401cde78e95977aa8c086dc0520c9cf60
Alexandre A. Bizri <alexandre@bizri.fr>
2017-03-05 23:33:17 +0000

    Bugfix for AvailableGroups and add corresponding test case

--- publishHelper.go
+++ publishHelper.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Find out what groups are available to the session
-func (s *Session) AvailableGroups() ([]Group,error) {
+func (s *Session) AvailableGroups() (map[Group]string,error) {
 	// Retrive main page
 	resp, err := s.Client.Get("http://iseplive.fr/")
 	if err != nil {
@@ -21,19 +21,24 @@ func (s *Session) AvailableGroups() ([]Group,error) {
 	}
 	
 	// Select groups
-	sel := doc.Find("publish-group-select").ChildrenFiltered("option")
-	
+	sel := doc.Find("#publish-group-select").ChildrenFiltered("option")
+
 	// Prepare return
 	// NB: There is always at least one option: 0 - None
-	groups := make([]Group, sel.Length())
+	groups := make(map[Group]string, sel.Length())
 	
 	// For each option, add to list
 	sel.Each(func(i int, s *goquery.Selection) {
+		// Retrieve group name
+		name := s.Text()
+	
+		// Retrieve group ID
 		rawgroup, _ := s.Attr("value")
 		group,_ := strconv.ParseUint(rawgroup,10,64)
-		groups[i]=Group(group)
+		
+		groups[Group(group)] = name
 	})
-	
+
 	// Return
 	return groups,err
 }
---
+++ publishHelper_test.go
@@ -0,0 +1,17 @@
+package goil
+
+import "testing"
+
+func Test_AvailableGroups(t *testing.T) {
+	skipIfNoSession(t)
+	
+	groups,err := session.AvailableGroups()
+	if err != nil{
+		t.Fatal(err)
+	}
+	
+	t.Log("These are the available groups")
+	for id,name := range(groups) {
+		t.Logf("%d: %s\n",id,name)
+	}
+}
